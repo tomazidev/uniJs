f3d834ebdff70bcddfa9aad31613f2b1d0518816
Tomasz Koprowski <tomasz.koprowski@talktalkplc.com>
2018-10-24 16:24:11 +0100

    Completing exercise 1 events and html bubbling

--- workshop5/exercise1/exercise1.js
+++ workshop5/exercise1/exercise1.js
@@ -8,6 +8,36 @@ myEl.addEventListener('click', function () {
 var docBody = document.body,
     parentElement = document.getElementById('parent'),
     childElement = document.getElementById('child');
+    trollElement = document.getElementById('troll');
+   
+    // Exercise 1 Part A
+    docBody.addEventListener('click', function (event) {
+        console.log('I am title');
+    });
+    parentElement.addEventListener('click', function (event) {
+        console.log('I am parent element');
+        event.stopPropagation();
+    });
+    childElement.addEventListener('click', function (event) {
+        console.log('I am child element');
+        event.stopPropagation();
+    });
+    trollElement.addEventListener('click', function (event) {
+        console.log('I am troll element');
+        event.stopPropagation();
+    });
+
+    // Exercise 1 Part B
+    parentElement.addEventListener('mouseleave', function (event) {
+        console.log('I am parent element');
+        event.stopPropagation();
+    });
+    childElement.addEventListener('mouseleave', function (event) {
+        console.log('I am child element');
+        event.stopPropagation();
+    });
+
+
 
 
 // Add your listeners here...
