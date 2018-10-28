10d67da743d58a8076cf2f0d21e6a3bf928a417b
Tomasz Koprowski <tomasz.koprowski@talktalkplc.com>
2018-10-28 15:13:04 +0000

    complet exercise 2 nd 3

--- workshop5/exercise2/exercise2.js
+++ workshop5/exercise2/exercise2.js
@@ -1,2 +1,8 @@
 // The element to attach the listener to
 var buttonWrapper = document.getElementById('button-wrapper');
+
+buttonWrapper.addEventListener('click', function (event) {
+    var attr = event.target.getAttribute('data-action');
+    console.log(attr);
+});
+
--- workshop5/exercise3/exercise3.js
+++ workshop5/exercise3/exercise3.js
@@ -1,56 +1,53 @@
-
-// Variables used by script.
-// We need references to the textarea, the button and the div to place the results in
 var userInput = document.getElementById('user-input'),
     saveButton = document.getElementById('btn'),
-    resultEl = document.getElementById('saved-text');
+    resultEl = document.getElementById('saved-text'),
+    counter = 0;
 
-// Helper function to help keep code clean...
-// All it does is make a button element, which we then add to each of the p 
-// elements we add to the page
-function makeMasterDeleteButton() {
+function makeMasterDeleteButton(counter) {
     var btn = document.createElement('a');
     btn.textContent = 'X';
-    btn.classList.add('item-delete'); // give button the class "item-delete"
+    btn.classList.add('item-delete');
     btn.setAttribute('href', '#');
+    btn.setAttribute('id', 'item_'+counter);
     return btn;
 }
 
+function makeNewItem() {
+    var noteElement = document.createElement('p');
+    noteElement.textContent = userInput.value;
+    return noteElement;
+}
 
-// Make sure we have valid elements to work with
+function joinItemAndButton (counter) {
+    var item = makeNewItem();
+    var button = makeMasterDeleteButton(counter);
+    item.insertAdjacentElement('beforeend' ,button);
+    return item;
+}
+
 if (userInput !== null && saveButton !== null && resultEl !== null) {
 
+    
     // Listen for clicks on form button
-    saveButton.addEventListener('click', function (event) {
+        saveButton.addEventListener('click', function (event) {
+            var item = makeNewItem();
+            var button = makeMasterDeleteButton(counter++);
+            var submitted = userInput.value;
 
-        var newNode, // Element we will create
-            itemDeleteButton, // Delete button for element
-            submitted = userInput.value; // Text from form field
+            if (submitted !== '') {
+                item.insertAdjacentElement('beforeend', button);
+                resultEl.appendChild(item);
+                
+                // Listen for clicks on delete button
+                button.addEventListener('click', function () {
+                    resultEl.removeChild(item);
+                })
+        
+                userInput.value = '';
+            }
 
-        // Check if user entered something
-        if (submitted !== '') {
-
-            // Make a p element and add the submitted text to it
-            newNode = document.createElement('p');
-            newNode.textContent = submitted;
-
-            // Make a delete button element and append it to p element
-            itemDeleteButton = makeMasterDeleteButton();
-            newNode.insertAdjacentElement('afterbegin', itemDeleteButton);
-
-            // add the p element to the page
-            resultEl.appendChild(newNode);
-
-            // Erase user's text from the textarea
-            userInput.value = '';
-        }
-
-        // Stop the browser's default behaviour. By default, when a "button" within a "form"
-        // element is clicked, the form gets submitted, resulting in a page reload (which we don't want)
-        event.preventDefault();
-    });
-
-    // Add event listener for item deletions here...
+            event.preventDefault();
+        });
 
 }
 
