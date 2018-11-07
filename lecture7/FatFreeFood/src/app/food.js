ceb39470bc0f34f257281fa406e0e05994202ed9
Tomasz Koprowski <tomasz.koprowski@talktalkplc.com>
2018-11-07 14:25:32 +0000

    babel and webpack messing around

---
+++ lecture7/FatFreeFood/src/app/food.js
@@ -0,0 +1,21 @@
+"use strict";​
+// Food is a base class​
+export class Food {​
+    constructor (name, protein, carbs, fat) {​
+        this.name = name;​
+        this.protein = protein;​
+        this.carbs = carbs;​
+        this.fat = fat;​
+   }​
+
+   toString () {​
+       return `${this.name} | ${this.protein}g P :: ${this.carbs}g C :: ${this.fat}g F`​
+   }​
+
+   print () {​
+     console.log( this.toString() );​
+   }​
+
+}​
+
+​
---
+++ lecture7/FatFreeFood/src/index.js
@@ -0,0 +1,4 @@
+import { Food } from './app/food.js';
+
+const troll = new Food('Chicken Brest', 26, 0 ,3.5);
+troll.print(); // 'Chicken Breast | 26g P :: 0g C :: 3.5g F'​
